97255798c635f6b7400933d884f8b6d90fb02403
andresabcb <andresabcb@al.insper.edu.br>
2021-05-20 11:59:57 -0300

    parte 2 funcionando

--- Projeto8/parte2.py
+++ Projeto8/parte2.py
@@ -21,7 +21,8 @@ from funcoes_LPF import *
 def main():
     signal = signalMeu()
     tempo = 3
-    portadora = 20000
+    portadora = 20000 # diminuir p 14000 se nao der
+    print(f'tipo da portadora {type(portadora)}')
 
     ## ABRINDO O ARQUIVO
     audio = 'wav/modularizado_leo.wav'
@@ -30,22 +31,42 @@ def main():
     print(f'lista audio: {audio_modulado}')
     print(f'samplerate: {samplerate}')
     # lista_audio = audio_modulado
-    lista_audio = np.ravel(audio_modulado) # deixando pronto para usar
-    print(type(lista_audio))
-    time_audio = int(len(lista_audio)/samplerate) # CONSERTAR
+    play_audio(audio_modulado,samplerate,'sinal modulado')
 
-    signal.plotFFT(lista_audio,samplerate,'Áudio modulado')
+    ampl = 1
+    x1, sin1 = signal.generateSin(portadora,ampl,tempo,samplerate)
+
+    # teste
+    lista_audio = audio_modulado
+    print(lista_audio)
+    # lista_audio = np.ravel(audio_modulado) # deixando pronto para usar
+    # plt.plot(lista_audio)
+    print(f'tipo da lista_audio {type(lista_audio)}')
+    signal.plotFFT(lista_audio,samplerate,'lista audio') # teste
+
+    print(type(lista_audio))
+
+    # signal.plotFFT(lista_audio,samplerate,'Áudio modulado')
     ## verificar se está entre 16 e 24 Hz
 
     ## DEMODULANDO
-    sinal_demodulado = lista_audio * portadora
+    # sinal_demodulado = []
+    # for i in range(len(lista_audio)):
+    #     sinal_demodulado.append(lista_audio[i] * portadora)
+    sinal_demodulado = lista_audio * sin1
+    signal.plotFFT(sinal_demodulado,samplerate,'demodulado 1')
 
     ## FILTRANDO
     cutoff = 4000 #Hz
     sinal_filtrado = filtro(sinal_demodulado, samplerate, cutoff)
-    t = np.linspace(0,tempo,len(lista_audio))
+    t = np.linspace(0,tempo,len(sinal_filtrado))
     ## ver se é para executar mesmo o do demodulado
-    plot_and_play(signal,sinal_demodulado,t,samplerate,'Sinal Demodulado')
+    # plot_and_play(signal,sinal_filtrado,t,samplerate,'Sinal Demodulado')
+
+    # teste
+    plota_graficos(t,sinal_filtrado,'demodulado 2')
+    signal.plotFFT(sinal_filtrado,samplerate,'demodulado 2')
+    play_audio(sinal_filtrado, samplerate,'demodulado 2')
 
 if __name__ == "__main__":
     main()
